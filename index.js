65557ec6f8ae781271c917aa37ff7006c4526e2b
Tsin Moua <tsinmoua@gmail.com>
2020-10-24 21:31:48 -0500

    Added inquirer questions

--- index.js
+++ index.js
@@ -3,17 +3,65 @@ var inquirer = require("inquirer");
 
 // create the connection information for the sql database
 var connection = mysql.createConnection({
-  host: "localhost",
+    host: "localhost",
 
-  port: 3306,
+    port: 3306,
 
-  user: "root",
-  password: "root",
+    user: "root",
+    password: "root",
 
-  database: "employee_tracker_db"
+    database: "employee_tracker_db"
 });
 
-connection.connect(function(err) {
+connection.connect(function (err) {
     if (err) throw err;
     start();
-  });
+});
+
+function start() {
+    inquirer
+        .prompt({
+            name: "action",
+            type: "list",
+            message: "What would you like to do?",
+            choices:
+                [
+                    "Add a department",
+                    "Add a role",
+                    "Add an employee",
+                    "View departments",
+                    "View roles",
+                    "View employees",
+                    "Update employee roles",
+                    "EXIT"
+                ]
+        })
+        .then(function (answer) {
+            switch (answer.action) {
+                case "Add a department":
+                    addADepartment();
+                    break;
+                case "Add a role":
+                    addARole();
+                    break;
+                case "Add an employee":
+                    addAnEmployee();
+                    break;
+                case "View departments":
+                    viewDepartments();
+                    break;
+                case "View roles":
+                    viewroles();
+                    break;
+                case "View employees":
+                    viewEmployees();
+                    break;
+                case "Update employee roles":
+                    updateEmployeeRoles();
+                    break;
+                case "EXIT":
+                    console.log("You have exited the application");
+                    connection.end();
+            }
+        });
+}
