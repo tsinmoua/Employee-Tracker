322f2a2dc054cd068a3cb26b48f3ec11a79d333e
Tsin Moua <tsinmoua@gmail.com>
2020-10-25 17:39:38 -0500

    Finished addAnEmployee function

--- index.js
+++ index.js
@@ -64,7 +64,6 @@ function start() {
 }
 
 function addADepartment() {
-    // console.log("You have chosen: Add a department");
     inquirer.prompt(
         {
             name: "department",
@@ -81,7 +80,7 @@ function addADepartment() {
                     [
                         "Yes",
                         "No, go back",
-                        "I don't want to add a department anymore"
+                        "Main Menu"
                     ]
             }
         ).then(function (answer2) {
@@ -105,7 +104,7 @@ function addADepartment() {
                     );
                 case "No, go back":
                     return addADepartment();
-                case "I don't want to add a department anymore":
+                case "Main Menu":
                     return start();
             }
         });
@@ -119,10 +118,9 @@ function addARole() {
     connection.query("SELECT * FROM department ORDER BY id", function (err, res) {
         if (err) throw err;
         res.forEach(element => {
-            deptArray.push({id: element.id, name: element.name});
+            deptArray.push({ id: element.id, name: element.name });
         });
     })
-
 
     inquirer.prompt([
         {
@@ -148,57 +146,185 @@ function addARole() {
             choices: deptArray
         }
     ])
-    .then(function (answer) {
+        .then(function (answer) {
+            inquirer.prompt(
+                {
+                    name: "confirm",
+                    type: "rawlist",
+                    message: `Are you sure want to add the role: ${capFirstLetter(answer.role)}, $${answer.salary}, ${answer.departmentId}`,
+                    choices:
+                        [
+                            "Yes",
+                            "No, go back",
+                            "Main Menu"
+                        ]
+                }
+            ).then(function (answer2) {
+                switch (answer2.confirm) {
+                    case "Yes":
+
+                        console.log("--------------------ID------------------");
+                        // console.log(answer.departmentId.id);
+                        let chosenDept = answer.departmentId
+                        // console.log(chosenDept);
+                        const deptID = function (dept) {
+                            for (let i = 0; i < deptArray.length; i++) {
+                                if (dept === deptArray[i].name) {
+                                    return deptArray[i].id
+                                }
+                            }
+                        }
+                        // console.log(deptID(chosenDept));
+
+                        connection.query("INSERT INTO role SET ?",
+                            {
+                                title: capFirstLetter(answer.role),
+                                salary: answer.salary,
+                                department_id: deptID(chosenDept)
+                            },
+                            function (err, res) {
+                                if (err) {
+                                    console.log(`\n \n There is already a role named "${capFirstLetter(answer.role)}". You will be taken back to the main menu \n`);
+                                    return start();
+                                } else {
+                                    console.log(`\n \n ${res.affectedRows} role named "${capFirstLetter(answer.role)}" has been added!\n`);
+                                    return start();
+                                }
+                            }
+                        );
+                    case "No, go back":
+                        return addARole();
+                    case "Main Menu":
+                        return start();
+                }
+            });
+        });
+}
+
+
+function addAnEmployee() {
+
+    let roleArray = [];
+
+    connection.query("SELECT id, title FROM role ORDER BY id", function (err, res) {
+        if (err) throw err;
+        // console.log(res);
+        res.forEach(element => {
+            roleArray.push({ id: element.id, name: element.title });
+        });
+        // console.log(roleArray);
+    })
+
+    let managerArray = [];
+
+    connection.query("SELECT id, first_name, last_name FROM employee ORDER BY id", function (err, res) {
+        if (err) throw err;
+        // console.log(res);
+        res.forEach(element => {
+            managerArray.push({ id: element.id, name: element.first_name + " " + element.last_name });
+
+        });
+        managerArray.push("This employee doesn't have a manager")
+        // console.log(managerArray);
+    })
+
+    inquirer.prompt([
+        {
+            name: "firstName",
+            type: "input",
+            message: "What is the employee's first name?",
+            validate: input => {
+                // console.log(input.match(/\d/g));
+                if (input !== "" && input.match(/\d/g) === null) {
+                    return true;
+                }
+                return "Please enter a valid first name."
+            }
+        },
+        {
+            name: "lastName",
+            type: "input",
+            message: "What is the employee's last name?",
+            validate: input => {
+                // console.log(input.match(/\d/g));
+                if (input !== "" && input.match(/\d/g) === null) {
+                    return true;
+                }
+                return "Please enter a valid last name."
+            }
+        },
+        {
+            name: "roleID",
+            type: "list",
+            message: "What is the employee's role?",
+            choices: roleArray
+        },
+        {
+            name: "managerID",
+            type: "list",
+            message: "Who is the employee's manager?",
+            choices: managerArray
+        }
+    ]).then(function (answer) {
         inquirer.prompt(
             {
                 name: "confirm",
                 type: "rawlist",
-                message: `Are you sure want to add the role: ${capFirstLetter(answer.role)}, $${answer.salary}, ${answer.departmentId}`,
+                message: `Are you sure want to add the employee: ${capFirstLetter(answer.firstName)} ${capFirstLetter(answer.lastName)}, ${answer.roleID}, with the manager: ${answer.managerID}`,
                 choices:
                     [
                         "Yes",
                         "No, go back",
-                        "I don't want to add a role anymore"
+                        "Main Menu"
                     ]
             }
         ).then(function (answer2) {
             switch (answer2.confirm) {
                 case "Yes":
 
-                console.log("--------------------ID------------------");
-                // console.log(answer.departmentId.id);
-                let chosenDept = answer.departmentId
-                console.log(chosenDept);
-                const deptID = function (x){
-                    for (let i = 0; i < deptArray.length; i++) {
-                        if (x === deptArray[i].name) {
-                            // console.log(deptArray[i].id);
-                            // console.log(deptArray[i].name);
-                            return deptArray[i].id
+                    let chosenRole = answer.roleID
+                    // console.log(chosenRole);
+                    const roleID = function (role) {
+                        for (let i = 0; i < roleArray.length; i++) {
+                            if (role === roleArray[i].name) {
+                                return roleArray[i].id
+                            }
                         }
                     }
-                }
-                console.log(deptID);
+                    // console.log(roleID(chosenRole));
 
-                    connection.query("INSERT INTO role SET ?",
+                    let chosenManager = answer.managerID
+                    // console.log(chosenManager);
+                    const managerID = function (manager) {
+                        for (let i = 0; i < managerArray.length; i++) {
+                            if (manager === managerArray[i].name) {
+                                return managerArray[i].id
+                            } else if (manager === "This employee doesn't have a manager") {
+                                return null;
+                            }
+                        }
+                    }
+                    // console.log(managerID(chosenManager));
+
+                    return connection.query("INSERT INTO employee SET ?",
                         {
-                            title: capFirstLetter(answer.role),
-                            salary: answer.salary,
-                            department_id: deptID(chosenDept)
+                            first_name: capFirstLetter(answer.firstName),
+                            last_name: capFirstLetter(answer.lastName),
+                            role_id: roleID(chosenRole),
+                            manager_id: managerID(chosenManager)
                         },
                         function (err, res) {
-                            if (err) {
-                                console.log(`\n \n There is already a role named "${capFirstLetter(answer.role)}". You will be taken back to the main menu \n`);
-                                return start();
-                            } else {
-                                console.log(`\n \n ${res.affectedRows} role named "${capFirstLetter(answer.role)}" has been added!\n`);
+                            if (err){
+                                console.log(err);
                                 return start();
                             }
+                            console.log(`\n ${res.affectedRows} employee named "${capFirstLetter(answer.firstName)} ${capFirstLetter(answer.lastName)}" has been added!\n`);
+                            return start();
                         }
                     );
                 case "No, go back":
-                    return addARole();
-                case "I don't want to add a role anymore":
+                    return addAnEmployee();
+                case "Main Menu":
                     return start();
             }
         });
